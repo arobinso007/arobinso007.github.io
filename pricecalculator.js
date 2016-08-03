453344fc7de7fb27891b0d32cfe87357dc370850
arobinso007 <arobinso007@gmail.com>
2016-08-02 20:51:18 -0700

    Update pricecalculator.js

--- pricecalculator.js
+++ pricecalculator.js
@@ -3,14 +3,15 @@ var water;
 var materials;
 var labor;
 var lb2gram = .00220462;
+var gram2oz = 28.3495;
 var yield;
 var cost;
 var netGramPrice;
 var el;
 
-function totalCost(){
-	
-}
+//function totalCost(){
+//	
+//}
 
 function breakeven(){
 	yield = document.breakevenForm.yield.value;
@@ -22,7 +23,24 @@ function breakeven(){
 	netGramPrice = cost/(yield/lb2gram);
 //	document.breakevenForm.price.value = netprice;
 	el = document.getElementById("gramBEP");
-	el.textContent = netGramPrice;
+	el.textContent ="$" + netGramPrice.toFixed(2);
+	el = document.getElementById("eigthOZ");
+	el.textContent ="$" + (0.125*gram2oz*netGramPrice).toFixed(2);
+	el = document.getElementById("quarterOZ");
+	el.textContent ="$" + (0.25*gram2oz*netGramPrice).toFixed(2);
+	el = document.getElementById("halfOZ");
+	el.textContent ="$" + (0.5*gram2oz*netGramPrice).toFixed(2);
+	el = document.getElementById("oneOZ");
+	el.textContent ="$" + (gram2oz*netGramPrice).toFixed(2);
+	
+	
+	el = document.getElementById("quarterLB");
+	el.textContent ="$" + ((0.25/lb2gram)*netGramPrice).toFixed(2);
+	el = document.getElementById("halfLB");
+	el.textContent ="$" + ((0.5/lb2gram)*netGramPrice).toFixed(2);
+	el = document.getElementById("oneLB");
+	el.textContent ="$" + ((1/lb2gram)*netGramPrice).toFixed(2);
+	
 	
 	el = document.getElementById("nickelBES");
 	el.textContent = (5/netGramPrice).toFixed(3) + "g";
